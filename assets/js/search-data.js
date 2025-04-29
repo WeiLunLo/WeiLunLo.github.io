d2a9878bb14a86d1cecc611282c5fe39bb260628
WeiLunLo <weilunlotw@gmail.com>
2025-04-29 17:45:49 +0000

    Deploying to gh-pages from @ WeiLunLo/WeiLunLo.github.io@4f79b9f5a56dddaea3404602e08fe5c2c4ac3628 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,14 +459,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%77%6C%6F%35@%75%72.%72%6F%63%68%65%73%74%65%72.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -480,14 +473,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=OJHSxAIAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
